26409f47ab46399e5096dfe27b0f00bd135a03eb
nuryokusan <nuryokusan@gmail.com>
2018-09-17 11:04:58 +0200

    add neural-networks/simple-perceptron

---
+++ src/nature_of_code/neural_networks/simple_perceptron/sketch.cljs
@@ -0,0 +1,193 @@
+(ns nature-of-code.neural-networks.simple-perceptron.core
+  "Neural Networking - A Simple Perceptron learns to classify 2D-Points
+  according to a linear Function
+  Based on the Nature of Code by Daniel Shiffman http://natureofcode.com"
+  (:require [quil.core :as q :include-macros true]
+            [quil.middleware :as m]
+            [goog.string :as gstring]
+            [goog.string.format :as gformat]))
+
+(def params
+  {:size-x 800
+   :size-y 200
+   :background 255
+   :frame-rate 30
+   :x-min -400
+   :x-max 400
+   :y-min -100
+   :y-max 100
+   :point-r 8
+   :inputs-per-neuron 3
+   :learning-rate 0.00001
+   :training-record-count 2000
+   :f-line-color 127
+   :n-line-color 0})
+
+;;
+;; Protocols
+;;
+
+(defprotocol NeuralNetwork
+  (train [this inputs desired] "train the neural network")
+  (activate [this sum]))
+
+(defprotocol FeedForward
+  (feed-forward [this inputs]))
+
+;;
+;; Perceptron
+;;
+
+(defn random [min max]
+  (+ min (rand (- max min))))
+
+(defn f [x]
+  "The function that describes a line"
+  (+ (* 0.4 x) 1))
+
+(defrecord Perceptron [weights learning-rate]
+  NeuralNetwork
+  (train [perceptron inputs desired]
+    ; guess the result (0, -2, or 2) and mult by learning-rate
+    (let [guess (feed-forward perceptron inputs)
+          error (- desired guess)
+          next-weights (into []
+                             (map
+                               #(+ %1 (* %2 error (:learning-rate perceptron)))
+                               (:weights perceptron)
+                               inputs))]
+      ;(dbg (str error next-weights))
+      (assoc perceptron :weights next-weights)))
+
+  (activate [perceptron sum]
+    (if (> sum  0)
+      1
+      -1))
+
+  FeedForward
+  (feed-forward [perceptron inputs]
+    (let [sum (reduce
+                +
+                (map * inputs (:weights perceptron)))]
+      (activate perceptron sum))))
+
+(defn random-weights [weights-count]
+  (into [] (take weights-count (repeatedly #(random -1 1)))))
+
+(defn gen-perceptron
+  [& {:keys [weights learning-rate]
+      :or {weights [] learning-rate 0.0}}]
+  (Perceptron. weights learning-rate))
+
+;;
+;; TrainingRecord
+;;
+
+(defrecord TrainingRecord [inputs answer])
+
+(defn gen-training-record
+  [answer & inputs]
+  (TrainingRecord. (vec inputs) answer))
+
+(defn gen-training-data [training-record-count]
+  (into []
+        (take training-record-count
+              (repeatedly
+                #(let [
+                       x (random (params :x-min) (params :x-max))
+                       y (random (params :x-min) (params :x-max))
+                       answer (if (< y (f x)) -1 1)]
+                   (gen-training-record answer x y 1))))))
+
+;;
+;; Sketch
+;;
+
+(def sketch-model
+  (atom
+    {:training-data nil
+     :training-index 0
+     :perceptron nil}))
+
+(defn init-sketch-model [m-atom]
+  (swap! m-atom #(assoc % :training-data (gen-training-data (params :training-record-count))))
+  (let [weights (random-weights (params :inputs-per-neuron))]
+    (swap! m-atom #(assoc % :perceptron (gen-perceptron :weights weights :learning-rate (params :learning-rate))))))
+
+(defn setup-sketch []
+  (q/frame-rate (params :frame-rate))
+  (q/smooth)
+  (init-sketch-model sketch-model))
+
+(defn draw-sketch []
+  ; draw Background
+  (q/background (params :background))
+  (q/translate (/ (q/width) 2) (/ (q/height) 2))
+
+  ; Draw the line
+  (q/stroke (params :n-line-color))
+  (q/stroke-weight 4)
+  (let [x1 (params :x-min)
+        y1 (f  x1)
+        x2 (params :x-max)
+        y2 (f x2)]
+    (q/line x1 y1 x2 y2))
+
+  ; Draw the line based on the current weights
+  ; Formula is weights[0]*x + weights[1]*y + weights[2] = 0
+  (q/stroke (params :n-line-color))
+  (q/stroke-weight 1)
+  (let [perceptron (:perceptron @sketch-model)
+        weights (:weights perceptron)
+        x1 (params :x-min)
+        y1 (- (- (nth weights 2)) (/ (* (nth weights 0) x1) (nth weights 1)))
+        x2 (params :x-max)
+        y2 (- (- (nth weights 2)) (/ (* (nth weights 0) x2) (nth weights 1)))]
+    (q/line x1 y1 x2 y2))
+
+  (let [perceptron (:perceptron @sketch-model)
+        training-data (:training-data @sketch-model)
+        training-index (:training-index @sketch-model)]
+    ; Train the Perceptron with one "training" point at a time
+    (let [inputs (:inputs (nth training-data training-index))
+          answer (:answer (nth training-data training-index))
+          next-perceptron (train perceptron inputs answer)
+          next-training-index (mod (inc training-index) (count training-data))]
+      (swap! sketch-model #(assoc % :perceptron next-perceptron :training-index next-training-index)))
+
+    ; Draw all the points based on what the Perceptron would "guess"
+    ; Does not use the "known" correct answer
+    (loop [index 0]
+      (q/stroke 0)
+      (q/stroke-weight 1)
+      (q/fill 0)
+
+      (let [training-record (nth training-data index)
+            inputs (:inputs training-record)
+            x (nth inputs 0)
+            y (nth inputs 1)
+            guess (feed-forward perceptron inputs)]
+        (when (> guess 0)
+          (q/no-fill))
+        (q/ellipse x y (params :point-r) (params :point-r)))
+      (when (< index training-index)
+        (recur (inc index))))
+
+    ; Display some info
+    (q/translate (/ (- (q/width)) 2) (/ (+ (q/height)) 2))
+    (q/fill 127)
+    (let [[w0 w1 w2] (:weights perceptron)
+          x (/ w0 w1)
+          c (/ w2 w1)]
+      ;(q/text (str training-index x c) 10 -18))))
+      (q/text (str training-index (gstring/format "y = %.3f * x + %.3f" x c)) 10 -18))))
+
+; This sketch uses functional-mode middleware.
+; Check quil wiki for more info about middlewares and particularly
+; fun-mode.
+(q/defsketch simple-perceptron
+  :host "simple-perceptron"
+  :size [(params :size-x) (params :size-y)]
+  :setup setup-sketch
+  :draw draw-sketch
+  :middleware [m/fun-mode])
