fc2120266e03798421736845dd45c6570096b54b
nuryokusan <nuryokusan@gmail.com>
2018-09-18 15:47:55 +0200

    make simple-perceptron work

--- src/nature_of_code/neural_networks/simple_perceptron/sketch.cljs
+++ src/nature_of_code/neural_networks/simple_perceptron/sketch.cljs
@@ -7,6 +7,10 @@
             [goog.string :as gstring]
             [goog.string.format :as gformat]))
 
+;;
+;; config
+;;
+
 (def config
   {:background 255
    :frame-rate 30
@@ -21,91 +25,83 @@
    :f-line-color 127
    :n-line-color 0})
 
-;;
-;; Protocols
-;;
+(def reciprocal (partial / 1))
 
-(defprotocol NeuralNetwork
-  (train [this inputs desired] "train the neural network")
-  (activate [this sum]))
+(def epow (partial Math.pow Math/E))
 
-(defprotocol FeedForward
-  (feed-forward [this inputs]))
+(def sigmoid (comp reciprocal inc epow -))
 
 ;;
 ;; Perceptron
 ;;
 
-(defn random [min max]
-  (+ min (rand (- max min))))
+(defn gen-perceptron
+  [& {:keys [weights learning-rate] :as m}]
+  m)
 
 (defn f [x]
   "The function that describes a line"
   (+ (* 0.4 x) 1))
 
-(defrecord Perceptron [weights learning-rate]
-  NeuralNetwork
-  (train [perceptron inputs desired]
-    ; guess the result (0, -2, or 2) and mult by learning-rate
-    (let [guess (feed-forward perceptron inputs)
-          error (- desired guess)
-          next-weights (into []
-                             (map
-                               #(+ %1 (* %2 error (:learning-rate perceptron)))
-                               (:weights perceptron)
-                               inputs))]
-      ;(dbg (str error next-weights))
-      (assoc perceptron :weights next-weights)))
-
-  (activate [perceptron sum]
-    (if (> sum  0)
-      1
-      -1))
-
-  FeedForward
-  (feed-forward [perceptron inputs]
-    (let [sum (reduce
-                +
-                (map * inputs (:weights perceptron)))]
-      (activate perceptron sum))))
+(defn random [min max]
+  (+ min (rand (- max min))))
 
 (defn random-weights [weights-count]
   (into [] (take weights-count (repeatedly #(random -1 1)))))
 
-(defn gen-perceptron
-  [& {:keys [weights learning-rate]
-      :or {weights [] learning-rate 0.0}}]
-  (Perceptron. weights learning-rate))
+#_(defn activate [sum]
+    "simple activate-function"
+    (if (> sum  0)
+      1
+      -1))
+
+(defn activate [sum]
+  "sigmoid activate-function (logistic function)"
+  (- 0.5 (sigmoid sum)))
+
+(defn feed-forward [perceptron inputs]
+  (let [sum (reduce
+             +
+             (map * inputs (:weights perceptron)))]
+    (activate sum)))
+
+(defn train [perceptron inputs desired]
+    ; guess the result (0, -2, or 2) and mult by learning-rate
+  (let [guess (feed-forward perceptron inputs)
+        error (- desired guess)
+        next-weights (into []
+                           (map
+                            (fn [weight input] (+ weight (* input error (config :learning-rate))))
+                            (:weights perceptron)
+                            inputs))]
+    ;;(js/console.log (str "error: " error " weights: " next-weights))
+    (assoc perceptron :weights next-weights)))
 
 ;;
 ;; TrainingRecord
 ;;
 
-(defrecord TrainingRecord [inputs answer])
-
 (defn gen-training-record
   [answer & inputs]
-  (TrainingRecord. (vec inputs) answer))
+  {:inputs (vec inputs) :answer answer})
 
 (defn gen-training-data [training-record-count]
   (into []
         (take training-record-count
               (repeatedly
-                #(let [
-                       x (random (config :x-min) (config :x-max))
-                       y (random (config :x-min) (config :x-max))
-                       answer (if (< y (f x)) -1 1)]
-                   (gen-training-record answer x y 1))))))
+               #(let [x (random (config :x-min) (config :x-max))
+                      y (random (config :x-min) (config :x-max))
+                      answer (if (< y (f x)) -1 1)]
+                  (gen-training-record answer x y 1))))))
 
 ;;
 ;; Sketch
 ;;
 
 (def sketch-model
-  (atom
-    {:training-data nil
-     :training-index 0
-     :perceptron nil}))
+  (atom {:training-data nil
+         :training-index 0
+         :perceptron nil}))
 
 (defn init-sketch-model [m-atom]
   (swap! m-atom #(assoc % :training-data (gen-training-data (config :training-record-count))))
@@ -113,6 +109,8 @@
     (swap! m-atom #(assoc % :perceptron (gen-perceptron :weights weights :learning-rate (config :learning-rate))))))
 
 (defn setup-sketch []
+  (js/console.log (str "setup-sketch " (q/width) " " (q/height)))
+  (js/console.log (str (sigmoid 100) "," (sigmoid 0) "," (sigmoid -100)))
   (q/frame-rate (config :frame-rate))
   (q/smooth)
   (init-sketch-model sketch-model))
@@ -165,17 +163,18 @@
             x (nth inputs 0)
             y (nth inputs 1)
             guess (feed-forward perceptron inputs)]
+        ;;(js/console.log (str "guess: " guess))
         (when (> guess 0)
           (q/no-fill))
         (q/ellipse x y (config :point-r) (config :point-r)))
       (when (< index training-index)
         (recur (inc index))))
 
-    ; Display some info
+    ; Display some info (text)
     (q/translate (/ (- (q/width)) 2) (/ (+ (q/height)) 2))
     (q/fill 127)
     (let [[w0 w1 w2] (:weights perceptron)
           x (/ w0 w1)
           c (/ w2 w1)]
-      ;(q/text (str training-index x c) 10 -18))))
+      ;;(q/text (str training-index x c) 10 -18))))
       (q/text (str training-index (gstring/format "y = %.3f * x + %.3f" x c)) 10 -18))))
