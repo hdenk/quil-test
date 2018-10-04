211ca03263fcc36a8af00022777f3b51f4279431
nuryokusan <nuryokusan@gmail.com>
2018-10-04 13:29:12 +0200

    smart-rockets-superbasic add doc-strings

--- src/nature_of_code/genetic_algorithms/smart_rockets_superbasic/sketch.cljs
+++ src/nature_of_code/genetic_algorithms/smart_rockets_superbasic/sketch.cljs
@@ -5,13 +5,15 @@
             [nature-of-code.math.vector :as mv]
             [nature-of-code.genetic-algorithms.smart-rockets-superbasic.spec :as spec]))
 
-(def config ^{:doc "DataStructure representing Params to customize the app"}
+(def config 
+  ^{:doc "DataStructure representing Params to customize the app"}
   {:background 255
    :frame-rate 30
    :lifetime 200
-   :fitness-fn :steps-and-distance
+   :fitness-fn :distance
    :mutation-rate 0.05
    :max-force 1.0
+   :mating-factor 100
    :target-r 24
    :rocket-count 50
    :rocket-r 4
@@ -23,21 +25,21 @@
 ;;
 
 (defn random-force
-  "gen random force"
+  "Gen random force (2D)"
   [force-limit]
   (let [angle (rand (* Math/PI 2))
         gene [(Math/cos angle) (Math/sin angle)]]
     (mv/multiply gene force-limit)))
 
 (defn gen-forces
-  "lifetime * random-force as vector)"
+  "Gen lifetime * random-force as vector"
   [lifetime]
   (let [forces (vec (repeatedly lifetime
                                 #(random-force (rand (config :max-force)))))]
     forces))
 
 (defn crossover-forces
-  "split two vectors and concat halves to a resulting vector"
+  "Split two vectors and concat halves to a resulting vector"
   ([forces-1 forces-2 split-idx] ; to support testing
    (let [resulting-forces (vec (concat ; @see https://stuartsierra.com/2015/04/26/clojure-donts-concat
                                 (first (split-at split-idx forces-1))
@@ -46,7 +48,9 @@
   ([forces-1 forces-2] ; called by app
    (crossover-forces forces-1 forces-2 (inc (rand-int (dec (count forces-1))))))) ; [1..n-1]
 
-(defn mutate-forces [forces mutation-rate]
+(defn mutate-forces 
+  "Replace forces (by chance) with random-force"
+  [forces mutation-rate]
   (let [next-forces (map (fn [force] (if (< (rand) mutation-rate)
                                        (random-force (rand (config :max-force)))
                                        force))
@@ -57,7 +61,9 @@
 ;; Rocket
 ;;
 
-(defn gen-rocket [m]
+(defn gen-rocket 
+  "Gen rocket, merge with default-attributes"
+  [m]
   (merge {:id nil
           :mass 1.0
           :location  nil
@@ -69,13 +75,17 @@
           :hit-target false}
          m))
 
-(defn gen-rocket-population [location forces rocket-count]
+(defn gen-rocket-population 
+  "Gen a population of rockets"
+  [location forces rocket-count]
   (mapv (fn [nr] (gen-rocket {:id (str "r" nr)
                               :location location
                               :forces (gen-forces (config :lifetime))}))
         (range rocket-count)))
 
-(defn move-rocket [rocket]
+(defn move-rocket 
+  "Progress rocket to next motion-state"
+  [rocket]
   (let [force (nth (:forces rocket) (:force-index rocket))
         acceleration (mv/divide force (float (:mass rocket)))
         next-location (mv/add (:location rocket) (:velocity rocket))
@@ -83,7 +93,9 @@
         next-force-index (inc (:force-index rocket))]
     (assoc rocket :location next-location :velocity next-velocity :force-index next-force-index)))
 
-(defn check-rocket-hit-target [rocket target]
+(defn check-rocket-hit-target
+  "Check if rocket hit target, sets attribute :hit-target" 
+  [rocket target]
   (let [d (mv/distance (:location rocket) (:location target))
         next-hit-target (< d (:target-r target))]
     (assoc rocket :hit-target next-hit-target)))
@@ -98,23 +110,21 @@
                       rockets)]
     next-rockets))
 
-(defmulti fitness (fn [key rocket & other] key))
+(defmulti fitness
+  "Fitness-Function is important, there maybe alternatives" 
+  (fn [key rocket & other] key))
 
 (defmethod fitness :steps [key rocket & _]
-  "Fitness-Kriterium is steps to target. Rockets that dont hit target get a fitness of 1"
+  "Fitness-Kriterium is steps to target, the fewer steps the fitter. Minimal fitness is 1"
   (Math/pow (max 1 (- (config :lifetime) (:force-index rocket))) 2))
 
 (defmethod fitness :distance [key rocket & [target]]
-  "Fitness-Kriterium is distance to target, the nearer the fitter the rocket"
+  "Fitness-Kriterium is distance to target, the nearer the fitter. Minimal fittnes is 1"
   (/ 1 (Math/pow (max 1 (mv/distance (:location rocket) (:location target))) 2)))
 
-(defmethod fitness :steps-and-distance [key rocket & [target]]
-  "Fitness-Kriterium is steps to target plus distance to target"
-  (let [f-steps (fitness :steps rocket)
-        f-distance (fitness :distance rocket target)]
-    (+ f-steps f-distance)))
-
-(defn update-population-fitness [rockets fitness-fn]
+(defn update-population-fitness 
+  "Update fitness for all rockets"
+  [rockets fitness-fn]
   (let [next-rockets (mapv
                       (fn [rocket]
                         (let [next-fitness (fitness-fn rocket)]
@@ -122,22 +132,30 @@
                       rockets)]
     next-rockets))
 
-(defn map-range [value from-min from-max to-min to-max]
+(defn map-range
+  "Transform a value from one range to another" 
+  [value from-min from-max to-min to-max]
   (+ to-min (* (- to-max to-min) (/ (- value from-min) (- from-max from-min)))))
 
-(defn reproduce-forces [rocket max-fitness]
+(defn reproduce-forces
+  "Produce 1 to :mating-factor forces (clone)" 
+  [rocket max-fitness]
   (let [norm-fitness (map-range (:fitness rocket) 0 max-fitness 0 1)
-        n (int (* norm-fitness 100))]
+        n (max 1 (int (* norm-fitness (config :mating-factor))))]
     ;(js/console.log (str "fitness:" (:fitness rocket) ", max-fitness:" max-fitness ", n:" n ", norm:" norm-fitness))
     (repeat n (:forces rocket))))
 
-(defn gen-mating-pool [rockets]
+(defn gen-mating-pool
+  "Gen a pool of forces (criterium is fitness)" 
+  [rockets]
   (let [max-fitness (:fitness (apply max-key :fitness rockets))]
     (vec (mapcat (fn [rocket]
                    (reproduce-forces rocket max-fitness))
                  rockets))))
 
-(defn select-next-population [rockets location]
+(defn select-next-population 
+  "Gen next population by 1. selection (random from mating-pool), 2. crossover, 3. mutation"
+  [rockets location]
   (let [mating-pool (gen-mating-pool rockets)
         pool-size (count mating-pool)]
     (mapv (fn [nr]
@@ -150,13 +168,17 @@
                            :forces forces})))
           (range (count rockets)))))
 
-(defn draw-rocket [rocket]
+(defn draw-rocket 
+  "Draw one rocket"
+  [rocket]
   (q/fill 200 100)
   (q/stroke 0)
   (q/rect-mode :center)
 
   (q/push-matrix)
-  (q/translate (first (:location rocket)) (second (:location rocket)))
+  ; Translate to rocket location
+  (let [[location-x location-y] (:location rocket)]
+    (q/translate location-x location-y))
   ; Draw a triangle rotated in the direction of velocity
   (let [theta (+ (mv/heading-2d (:velocity rocket)) (/ Math/PI 2))]
     (q/rotate theta))
@@ -178,7 +200,9 @@
     (q/end-shape :close)) ; processing.core.PConstants/CLOSE
   (q/pop-matrix))
 
-(defn draw-target [target]
+(defn draw-target 
+  "Draw target"
+  [target]
   (q/fill 0)
   (let [[location-x location-y] (:location target)
         target-r (:target-r target)]
@@ -188,7 +212,9 @@
 ;; Sketch
 ;;
 
-(def sketch-model (atom nil))
+(def sketch-model  
+  ^{:doc "Contains important data"}
+  (atom nil))
 
 (defn initial-rocket-location [[size-x size-y :as sketch-size]]
   (vector (/ size-x 2) (- size-y 20)))
@@ -196,7 +222,9 @@
 (defn initial-target-location [[size-x size-y :as sketch-size]]
   (vector (/ size-x 2) (config :target-r)))
 
-(defn init-sketch-model [m-atom]
+(defn init-sketch-model
+  "Initialize sketch-model, called by quil's setup"
+  [m-atom]
   (let [sketch-size (vector (q/width) (q/height))
         rocket-location (initial-rocket-location sketch-size)
         rocket-forces (gen-forces (config :lifetime))
@@ -209,13 +237,17 @@
                            :generation-count 0
                            :life-count 0}))))
 
-(defn setup-sketch []
+(defn setup-sketch 
+  "Setup sketch, Callback-Fn that gets called by quil, must be registered"
+  []
   (js/console.log (str "setup-sketch " (q/width) " " (q/height)))
   (q/frame-rate (config :frame-rate))
   (q/smooth)
   (init-sketch-model sketch-model))
 
-(defn draw-sketch []
+(defn draw-sketch 
+  "Draw sketch, Callback-Fn that gets called by quil, must be registered"
+  []
   ; draw Background
   (q/no-stroke)
   (q/fill 255)
@@ -250,5 +282,7 @@
   (q/text (str "Generation #: " (:generation-count @sketch-model)) 10 18)
   (q/text (str "Cycles left: " (- (config :lifetime) (:life-count @sketch-model))) 10 36))
 
-(defn mouse-pressed []
+(defn mouse-pressed 
+  "Mouse-Handler, Callback-Fn that gets called by quil, must be registered"
+  []
   (swap! sketch-model (fn [m] (assoc m :target {:location [(q/mouse-x) (q/mouse-y)] :target-r (config :target-r)}))))
